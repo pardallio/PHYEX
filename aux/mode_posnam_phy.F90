25e872dbf213a2e8e5dd32bbdd7e596408611a64
pardallio <pardalmiguel@hotmail.com>
2025-11-24 15:56:28 +0000

    adds compatibility with bundle build

--- aux/mode_posnam_phy.F90
+++ aux/mode_posnam_phy.F90
@@ -5,7 +5,7 @@ SUBROUTINE POSNAM_PHY(TFILENAM, CDNAML, LDNEEDNAM, LDFOUND)
 !Wrapper to call the AROME version of posnam
 
 USE MODD_IO, ONLY: TFILEDATA
-
+use namelist_mod
 IMPLICIT NONE
 
 TYPE(TFILEDATA),  INTENT(IN)    :: TFILENAM  !< Namelist file
@@ -13,7 +13,6 @@ CHARACTER(LEN=*), INTENT(IN)    :: CDNAML    !< Namelist name
 LOGICAL,          INTENT(IN)    :: LDNEEDNAM !< True to abort if namelist is absent
 LOGICAL,          INTENT(OUT)   :: LDFOUND   !< True if namelist has been found
 
-#include "posnam.intfb.h"
 CALL POSNAM(TFILENAM%NLU, CDNAML)
 LDFOUND=.TRUE. !Posnam aborts if not found
 
